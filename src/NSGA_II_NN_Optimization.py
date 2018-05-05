7212dda94280df1647518a94da4a0e9c84b97229
Javi <bigfirelp@gmail.com>
2018-05-05 18:01:09 +0200

    Added evaluation and selection (same as in FS)

--- src/NSGA_II_NN_Optimization.py
+++ src/NSGA_II_NN_Optimization.py
@@ -29,10 +29,56 @@ def InitializePopulation(pop_size, input_size, n_hidden):
 
 	return population	
 
-
 #-------------------- Population evaluation --------------------
 
+# Evaluates every individual of the given population using the
+# metrics contained in "objective_funcs".
+# "objective_funcs" is a list of tuples (function, [arguments])
+# meant to provide optional arguments to the evaluation functions
+# if needed (for example, data for model training).
+# It can also attempt to decrease computation time by means of
+# parallelism ("n_cores"), but if the metrics are too simple
+# it can lead to worse performance by overhead.
+def EvaluatePopulation(population, objective_funcs, n_cores = 1):
+
+	results = np.empty((population.shape[0],len(objective_funcs)))
+
+	if n_cores > multiprocessing.cpu_count():
+		n_cores = -1 # -1 means all cores for joblib.
+
+	with Parallel(n_jobs=n_cores) as parallel:
+
+		for o, (f, args) in enumerate(objective_funcs):
+
+			results[:,o] = parallel(delayed(f)(population[i,:],*args)
+							for i in range(population.shape[0]))
+
+	return results
+
 #-------------------- Selection process --------------------
+
+# Binary tournament. Takes as many random individuals as 2 * "pool_size"
+# and outputs "pool_size" winners as individuals selected for crossover.
+# For each pair, we choose the individual with the lower rank; if there's
+# a draw, we favor the one with greater crowding distance.
+# "sort_scores" contains (front, crowding distance) for each individual.
+# Returns an array of individuals.
+def TournamentSelection(population, sort_scores, pool_size):
+
+	selected = np.zeros((pool_size,population.shape[1]))
+	for i in range(selected.shape[0]):
+
+		candidates = choice(sort_scores.shape[0],replace=False,size=2)
+		best_front_pos = candidates[np.argmin(sort_scores[candidates,0])]
+
+		# If both fronts are the same, we can't use "best_front_pos".
+		if sort_scores[candidates[0]][0] != sort_scores[candidates[1]][0]:
+			selected[i] = population[best_front_pos]
+		else:
+			max_distance_pos = candidates[np.argmax(sort_scores[candidates,1])]
+			selected[i] = population[max_distance_pos]
+
+	return selected
 
 #-------------------- Crossover operators --------------------
 
