472307f05ea65920760aa246aa81fcdce84b2e62
Javi <bigfirelp@gmail.com>
2018-05-05 14:08:27 +0200

    Established parameters for NN optimization

--- src/NSGA_II_NN_Optimization.py
+++ src/NSGA_II_NN_Optimization.py
@@ -6,21 +6,54 @@ from sklearn.externals.joblib import Parallel, delayed
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import cohen_kappa_score, accuracy_score
 
-#-------------------- Crossover operators --------------------
-
 #-------------------- Population initialization --------------------
 
-#-------------------- Mutation operator --------------------
+#-------------------- Population evaluation --------------------
 
 #-------------------- Selection process --------------------
 
-#-------------------- Offspring generation --------------------
+#-------------------- Crossover operators --------------------
 
-#-------------------- Population evaluation --------------------
+#-------------------- Mutation operator --------------------
+
+#-------------------- Offspring generation --------------------
 
 #-------------------- Fitness metrics --------------------
 
 #-------------------- NSGA-II Algorithm --------------------
+
+# Main procedure of this module.
+# "data": a dictionary with two matrices of samples x features (train and test).
+# "labels": corresponding class labels for the samples in "data" (same order).
+# "max_hidden": upper bound for the number of hidden layers.
+# "objective_funcs": a list of Python functions for fitness evaluation.
+# "activation": a string with the Keras name of the activation function.
+# "pop_size": the working population size of the genetic algorithm.
+# "generations": how many generations the algorithm will run for.
+# "seed": random seed for reproducible experiments.
+# "crossover_prob": in practice, size(parents) x this = size(offspring).
+# "crossover_func": crossover method.
+# "mutation_prob": probability of a mutation after a successful crossover.
+# "mutation_func": mutation method.
+# "pool_fraction": proportion of parent pool size with respect to "pop_size".
+# "n_cores": number of processor cores used in the evaluation step.
+def NNOptimization(data, labels, max_hidden, objective_funcs, activation,
+		pop_size, generations, seed = 29,
+		crossover_prob = 0.9, crossover_func = None, 
+		mutation_prob = 0.8, mutation_func = None, 
+		pool_fraction = 0.5, n_cores = 1):
+
+	assert data['train'].shape[0] > 0, \
+			"You need a non-empty training set"
+	assert labels['train'].shape[0] == data['train'].shape[0], \
+			"You need an equal number of labels than of samples"
+	assert max_hidden > 0, \
+			"You need at least 1 hidden layer"
+	assert len(objective_funcs) > 1, \
+			"You need at least 2 objective functions."
+	assert pop_size >= 10, "You need at least 10 individuals"
+	assert generations >= 5, "You need at least 5 generations"
+	np.random.seed(seed)
 
 
 if __name__ == '__main__':
