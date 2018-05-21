b6b8eebd72feca58146386c36293720e8c474c0e
Javi <bigfirelp@gmail.com>
2018-05-21 17:00:39 +0200

    Added NSGA body for learning optimization

--- src/NSGA_II_NN_Learning_Optimization.py
+++ src/NSGA_II_NN_Learning_Optimization.py
@@ -172,3 +172,142 @@ def CrossValidationLoss(individual, data, labels, layers, activation,
 	return 1 - scores.mean()
 
 #-------------------- NSGA-II Algorithm --------------------
+
+#-------------------- NSGA-II Algorithm --------------------
+
+# Main procedure of this module.
+# "data": a dictionary with two matrices of samples x features (train and test).
+# "labels": corresponding class labels for the samples in "data" (same order).
+# "max_hidden": number of hidden layers.
+# "objective_funcs": a list of Python functions for fitness evaluation.
+# "activation": a string with the Keras name of the activation function.
+# "pop_size": the working population size of the genetic algorithm.
+# "generations": how many generations the algorithm will run for.
+# "seed": random seed for reproducible experiments.
+# "crossover_prob": in practice, size(parents) x this = size(offspring).
+# "crossover_func": crossover method.
+# "mutation_prob": probability of a mutation after a successful crossover.
+# "mutation_func": mutation method.
+# "pool_fraction": proportion of parent pool size with respect to "pop_size".
+# "n_cores": number of processor cores used in the evaluation step.
+def LearningOptimization(data, labels, max_epochs, lr_range, layers,
+		objective_funcs, activation, pop_size, generations, seed = 29,
+		crossover_prob = 0.2, crossover_func = ArithmeticMeanCrossover, 
+		mutation_prob = 0.8, mutation_func = GaussianMutation, 
+		pool_fraction = 0.5, n_cores = 1, show_metrics = False):
+
+	assert data['train'].shape[0] > 0, \
+			"You need a non-empty training set"
+	assert labels['train'].shape[0] == data['train'].shape[0], \
+			"You need an equal number of labels than of samples"
+	assert max_epochs > 0, \
+			"You need at least 1 training epoch"
+	assert len(lr_range) == 2 and 0 < lr_range[0] <= lr_range[1], \
+			"You need a valid learning rate range"
+	assert len(objective_funcs) > 1, \
+			"You need at least 2 objective functions"
+	assert pop_size >= 10, "You need at least 10 individuals"
+	assert generations >= 5, "You need at least 5 generations"
+	np.random.seed(seed)
+	K.tf.set_random_seed(seed)
+
+	if show_metrics:
+		print("Population size: "+str(pop_size))
+		print("Generations: "+str(generations))
+		print("Seed: "+str(seed))
+		print("Max epochs: "+str(max_epochs))
+		print("Learning rate range: "+str(lr_range))
+		print("Structure: "+str(layers))
+		print("Crossover probability: "+str(crossover_prob))
+		print("Mutation probability: "+str(mutation_prob))
+		print("Pool proportion (to population size): "+str(pool_fraction))
+
+	# As some evaluation functions need more arguments,
+	# put them together in a generic way for simplicity.
+	funcs_with_args = []
+	for f in objective_funcs:
+		funcs_with_args.append((f,[data,labels,layers,activation]))
+
+	# Pool size for parent selection.
+	pool_size = round(pop_size * pool_fraction)
+	# Preallocate intermediate population array (for allocation efficiency).
+	intermediate_pop = np.empty((pop_size+round(pool_size*(crossover_prob+mutation_prob)),
+										2),dtype=np.int32)
+	# Initial population.
+	population = InitializePopulation(pop_size,max_epochs,lr_range)
+	# Initial evaluation using objective_funcs.
+	evaluation = EvaluatePopulation(population,funcs_with_args,n_cores=n_cores)
+	K.clear_session()
+	# Initial non-domination scores [front, crowding_distance].
+	nds_scores = NonDominatedSortScores(evaluation)
+
+	if show_metrics:
+		print("Mean fitness values of each generation")
+		print(np.mean(evaluation,axis=0))
+
+	for gen in range(generations):
+
+		# Parents pool
+		parents = TournamentSelection(population,nds_scores[:pop_size],pool_size)
+		# Fill the intermediate population with previous generation + offspring.
+		intermediate_pop[:pop_size,:] = population
+		intermediate_pop[pop_size:,:] = CreateOffspring(parents,crossover_func,
+											mutation_func,crossover_prob,
+											mutation_prob)
+		# Apply evaluation and non-dominated sort to the joint population.
+		evaluation = EvaluatePopulation(intermediate_pop,funcs_with_args,n_cores=n_cores)
+		K.clear_session()
+		nds_scores = NonDominatedSortScores(evaluation)
+
+		# Sort population and scores based on front and crowding distance,
+		# then keep the best "pop_size" of them for the next generation.
+		nds_indices = IndirectSort(nds_scores)
+		population = intermediate_pop[nds_indices][:pop_size,:]
+		nds_scores = nds_scores[nds_indices][:pop_size,:]
+
+		if show_metrics:
+			print(np.mean(evaluation[nds_indices][:pop_size],axis=0))
+	
+	return population, nds_scores, evaluation[nds_indices][:pop_size,:]
+
+
+if __name__ == '__main__':
+	
+	# Pathnames assume that the script is called from parent directory
+	# and not from 'src' (data not included in the repository).
+
+	data_list = [{'train': np.load("data/data_training_104.npy"),
+				  'test': np.load("data/data_test_104.npy")},
+				 {'train': np.load("data/data_training_107.npy"),
+				  'test': np.load("data/data_test_107.npy")},
+				 {'train': np.load("data/data_training_110.npy"),
+				  'test': np.load("data/data_test_110.npy")}]
+
+	labels_list = [{'train': np.load("data/labels_training_104.npy"),
+				   'test': np.load("data/labels_test_104.npy")},
+				   {'train': np.load("data/labels_training_107.npy"),
+				   'test': np.load("data/labels_test_107.npy")},
+				   {'train': np.load("data/labels_training_110.npy"),
+				   'test': np.load("data/labels_test_110.npy")}]
+
+	features_list = [np.load("data/example_features_104.npy"),
+					 np.load("data/example_features_107.npy"),
+					 np.load("data/example_features_110.npy")]
+
+	for data, labels, features in zip(data_list,labels_list, features_list):
+
+		data['train'] = data['train'][:,features]
+		data['test'] = data['test'][:,features]
+		labels['train'] -= 1
+		labels['test'] -= 1
+		labels['train'] = to_categorical(labels['train'])
+		labels['test'] = to_categorical(labels['test'])
+
+		population, sort_scores, evaluation = \
+				LearningOptimization(data=data,labels=labels,max_epochs=300,
+				lr_range=[0.01,1],layers=np.asarray([76]),
+				objective_funcs=[KappaLoss,CrossValidationLoss],
+				activation="elu",pop_size=10,generations=5,seed=29,
+				crossover_prob=0.2, crossover_func=ArithmeticMeanCrossover, 
+				mutation_prob=0.8, mutation_func=GaussianMutation, 
+				pool_fraction=0.5, n_cores=1, show_metrics=True)
