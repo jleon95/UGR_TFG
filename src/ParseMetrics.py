39519e3bdd1d26e09782466b1530b4678259167b
Javi <bigfirelp@gmail.com>
2018-06-01 20:30:13 +0200

    Added functionality to export fitness loss evolution over time

---
+++ src/ParseMetrics.py
@@ -0,0 +1,25 @@
+import csv
+import re
+
+# Parses the human-readable output of the genetic algorithms
+# and saves the metric loss evolution to individual files.
+def ParseMetrics(metrics_file, output_prefix):
+
+	lines = open(metrics_file).readlines()
+	individuals = [x for x,y in enumerate(lines) if "Individual" in y]
+
+	for i in individuals:
+
+		number = lines[i].split()[1]
+
+		with open(output_prefix+number,"w") as output:
+
+			j = i+9
+			values = re.findall("\d.\d+",lines[j])
+
+			while values:
+
+				output.write(" ".join(values)+"\n")
+				j += 1
+				values = re.findall("\d.\d+",lines[j])
+
