2eabf2d4a3e3aebaf7cdd56d50e62bb639bc17bf
Javi <bigfirelp@gmail.com>
2018-06-17 13:19:00 +0200

    Redesigned sequential vs parallel FS experiment

--- src/FS_SequentialvsParallel.py
+++ src/FS_SequentialvsParallel.py
@@ -5,6 +5,7 @@
 from NSGA_II_Feature_SelectionSVM import *
 import numpy as np
 import time
+import multiprocessing
 
 if __name__ == '__main__':
 
@@ -14,39 +15,32 @@ if __name__ == '__main__':
 	labels = {'train': np.load("../data/labels_training_104.npy"),
 			  'test': np.load("../data/labels_test_104.npy")}
 
-	seeds = [29,28,20,11,26]
-	
-	time_sequential = 0
-	time_parallel = 0
+	cores = [x for x in range(2,multiprocessing.cpu_count()+1)]
 
-	for seed in seeds:
+	start_sequential = time.time()
 
-		start_sequential = time.time()
+	population, sort_scores, evaluation = \
+		FeatureSelection(data=data,labels=labels,max_features=50,
+			objective_funcs=[KappaLoss,CrossValidationLoss],
+			pop_size=100,generations=50,seed=29,crossover_prob=0.9,
+			crossover_func=UniformCrossover,mutation_prob=1.0,
+			mutation_func=FlipBitsMutation,pool_fraction=0.5,
+			n_cores=1,show_metrics=False)
 
-		population, sort_scores, evaluation = \
-			FeatureSelection(data=data,labels=labels,max_features=50,
-				objective_funcs=[KappaLoss,CrossValidationLoss],
-				pop_size=300,generations=150,seed=seed,crossover_prob=0.9,
-				crossover_func=UniformCrossover,mutation_prob=1.0,
-				mutation_func=FlipBitsMutation,pool_fraction=0.5,
-				n_cores=1,show_metrics=False)
+	print("Sequential:")
+	print(time.time() - start_sequential)
+	print("Parallel:")
 
-		time_sequential += time.time() - start_sequential
+	for n_core in cores:
 
 		start_parallel = time.time()
 
 		population, sort_scores, evaluation = \
 			FeatureSelection(data=data,labels=labels,max_features=50,
 				objective_funcs=[KappaLoss,CrossValidationLoss],
-				pop_size=300,generations=150,seed=seed,crossover_prob=0.9,
+				pop_size=100,generations=50,seed=29,crossover_prob=0.9,
 				crossover_func=UniformCrossover,mutation_prob=1.0,
 				mutation_func=FlipBitsMutation,pool_fraction=0.5,
-				n_cores=-1,show_metrics=False)
+				n_cores=n_core,show_metrics=False)
 
-		time_parallel += time.time() - start_parallel
-
-	print("Average of %d trials" % len(seeds))
-	print("Sequential:")
-	print(1.0 * time_sequential / len(seeds))
-	print("Parallel:")
-	print(1.0 * time_parallel / len(seeds))
+		print("%d %f" % (n_core,time.time() - start_parallel))
